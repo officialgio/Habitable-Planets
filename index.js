010a5e14bf838554593af11e12ab94028f1b2448
Giovanny Hernandez <87216285+officialgio@users.noreply.github.com>
2023-05-20 13:42:37 -0500

    Added project

---
+++ index.js
@@ -0,0 +1,37 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv") // reads raw data in bytes
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true, // return each row as a js object (k, v)
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.error(err);
+  })
+  .on("end", () => {
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+  });
